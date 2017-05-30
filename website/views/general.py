50b13b4b763a301a5e171ea01c047e49ffbdd423
AevumDecessus <aevum@decess.us>
2017-05-29 21:16:34 -0400

    Added initial files

---
+++ website/views/general.py
@@ -0,0 +1,22 @@
+from website import app
+from flask import Blueprint, render_template, session, redirect, url_for, \
+        request, abort
+
+mod = Blueprint('general', __name__)
+
+
+@mod.route('/about')
+def about(name=None):
+  return render_template('general/about.html', name=name)
+
+@mod.route('/what-we-believe')
+def believe(name=None):
+  return render_template('general/believe.html', name=name)
+
+@mod.route('/contact')
+def contact(name=None):
+  return render_template('general/contact.html', name=name)
+
+@mod.route('/team')
+def team(name=None):
+  return render_template('general/team.html', name=name)
